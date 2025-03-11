62cbb767713c7feafb0a304dc4b183fdc022979f
m4r122 <gomezamadocamila@gmail.com>
2025-03-11 07:36:19 -0500

    Conectado a docker

--- airport/src/test/java/com/unimagdalena/airport/repositories/AerolineaRepositoryTest.java
+++ airport/src/test/java/com/unimagdalena/airport/repositories/AerolineaRepositoryTest.java
@@ -6,32 +6,51 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
-import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.testcontainers.containers.PostgreSQLContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
 
-import java.util.Optional;
+import javax.sql.DataSource;
 
-import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
-import static org.junit.jupiter.api.Assertions.*;
+import static org.assertj.core.api.Assertions.assertThat;
+
 @ExtendWith(SpringExtension.class)
 @DataJpaTest
-class AerolineaRepositoryTest {
-    @Autowired
-    private TestEntityManager entityManager;
+@Testcontainers
+public class AerolineaRepositoryTest {
+
+    @Container
+    private static final PostgreSQLContainer<?> postgreSQLContainer =
+            new PostgreSQLContainer<>("postgres:16")
+                    .withDatabaseName("airport")
+                    .withUsername("postgres")
+                    .withPassword("traumsirene22");
 
     @Autowired
     private AerolineaRepository aerolineaRepository;
 
+    @Autowired
+    private DataSource dataSource;
+
+    @BeforeEach
+    public void setup() {
+        aerolineaRepository.deleteAll();
+
+        Aerolinea aerolinea1 = new Aerolinea();
+        aerolinea1.setNombre("Avianca");
+        aerolineaRepository.save(aerolinea1);
+
+        Aerolinea aerolinea2 = new Aerolinea();
+        aerolinea2.setNombre("Latam");
+        aerolineaRepository.save(aerolinea2);
+    }
+
     @Test
-    public void whenFindByNombre_thenReturnAerolinea() {
-        //given
-        Aerolinea aerolinea = new Aerolinea();
-        aerolinea.setNombre("Avianca");
-        entityManager.persist(aerolinea);
-        entityManager.flush();
-        //when
-        Aerolinea found = aerolineaRepository.findByNombre(aerolinea.getNombre());
-        //then
-        assertThat(found.getNombre()).isEqualTo(aerolinea.getNombre());
+    public void findByNombre() {
+        Aerolinea aerolinea = aerolineaRepository.findByNombre("Avianca");
+
+        assertThat(aerolinea).isNotNull();
+        assertThat(aerolinea.getNombre()).isEqualTo("Avianca");
     }
 }
